c8ceadb2b06b6821aa62c7489632fd68701448c8
Heather Caputo <caputoheather@ymail.com>
2019-04-30 13:50:56 +0000

    created blogpost examples and started styling

--- winebar/urls.py
+++ winebar/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from home.views import index
 from products import urls as urls_wines
 from home import urls as urls_home
+from blogposts import urls as urls_blogposts
 from django.views import static
 from .settings import MEDIA_ROOT
 
@@ -27,5 +28,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^wines/', include(urls_wines)),
     url(r'^home/', include(urls_home)),
+    url(r'^blog/', include(urls_blogposts)),
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT}),
 ]
