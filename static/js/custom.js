ec22928367776c9d5691e4902c09547acd18ffda
Heather Caputo <caputoheather@ymail.com>
2019-05-03 09:28:01 +0000

    altered search box for larger screens with more suited colour scheme

---
+++ static/js/custom.js
@@ -0,0 +1,8 @@
+// changes colour of nav on scroll to enable better visability
+
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".fixed-top");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  });
+});
