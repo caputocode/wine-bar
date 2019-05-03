047db97d34c01bfd4604c0d053f0070e3a922b22
Heather Caputo <caputoheather@ymail.com>
2019-05-03 08:20:13 +0000

    added mistral logo to index page

--- search/views.py
+++ search/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from products.models import Wine
+from django.db.models import Q
 # Create your views here.
 
 def do_search(request):
-    wines = Wine.objects.filter(name__icontains=request.GET['q'])
+    wines = Wine.objects.filter(Q(name__icontains=request.GET['q']) | Q(grape__icontains=request.GET['q']))
     return render(request, 'products.html', {'wines': wines})
