1e6ef6e28d1eb63434dd3854c5f953559c143936
Heather Caputo <caputoheather@ymail.com>
2019-06-19 15:29:47 +0000

    repo synced with travis for continuous integration testing

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Wine
 
 # Create your tests here.
+class WineTests(TestCase):
+    """
+    Define tests that run against Wine models
+    """
+
+    def test_str(self):
+        test_name = Wine(name='A wine')
+        self.assertEqual(str(test_name), 'A wine')
