d33b5afbac6feb16b680eb5c1f9de59af509d475
Heather Caputo <caputoheather@ymail.com>
2019-06-24 09:48:37 +0000

    tidied up code for home app pages and base.html and index.html, including commenting

--- products/views.py
+++ products/views.py
@@ -4,16 +4,16 @@ from .models import Wine
 
 def display_wines(request):
     """
-    View to show all boxes available
+    View to show all wines available
     """
     wines = Wine.objects.all().order_by('price')
-    return render(request, 'products.html', {'wines': wines})
+    return render(request, 'wines.html', {'wines': wines})
  
 def wine_detail(request, pk):
     """
-    Create a view that returns a single post object
-    based on the post ID (pk) and render it to the
-    'postdetail.html' template. Or return a 404 error
+    Create a view that returns a single wine object
+    based on the wine ID (pk) and render it to the
+    'wine-detail.html' template. Or return a 404 error
     if the post is not found
     """
     wine = get_object_or_404(Wine, pk=pk)
