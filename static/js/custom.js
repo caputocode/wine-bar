aec78011ec7b07b6c6c1a161d2c43219d5080393
Heather Caputo <caputoheather@ymail.com>
2019-06-24 12:47:52 +0000

    tidied and commented code in several areas

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,8 +1,25 @@
-// changes colour of nav on scroll to enable better visability
+// fixed top nav on scroll
 
-$(function () {
-  $(document).scroll(function () {
+$(function() {
+  $(document).scroll(function() {
     var $nav = $(".fixed-top");
     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
   });
 });
+
+// enables modal pop up on click of search icon in navbar 
+
+var modal = document.getElementById('boxModal');
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+  modal.style.display = "block";
+};
+span.onclick = function() {
+  modal.style.display = "none";
+};
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
